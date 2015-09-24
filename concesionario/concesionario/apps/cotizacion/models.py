6dbd7611b01e5ab34e3836375a07c838933af21f
BryanTabarez <userbryan@gmail.com>
2015-09-24 00:11:46 -0500

    Implementacion modelos...

--- concesionario/concesionario/apps/cotizacion/models.py
+++ concesionario/concesionario/apps/cotizacion/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from concesionario.apps.cliente.models import Cliente
+from concesionario.apps.vehiculo.models import Vehiculo
 
 # Create your models here.
+class Cotizacion(models.models):
+	id_cotizacion = models.AutoField(primary_key=True)
+	empleado = models.OneToOneField(User)
+	cliente = models.OneToOneField(Cliente)
+	vehiculo = models.OneToOneField(Vehiculo)
+	fecha = models.DateTimeField()
+	fecha_vencimiento = models.DateTimeField()
+
+	CREDITO = 'Credito'
+	EFECTIVO = 'Efectivo'
+	TARJETA_CREDITO = 'Tarjeta_credito'
+	TARJETA_DEBITO = 'Tarjeta_debito'
+
+	tipo = (
+		(CREDITO, 'Credito'),
+		(EFECTIVO, 'Efectivo'),
+		(TARJETA_CREDITO, 'Tarjeta de credito'),
+		(TARJETA_DEBITO, 'Tarjeta de debito'),
+	)
+
+	forma_pago = models.CharField(max_length=20, choices=tipo, default=EFECTIVO
--- concesionario/concesionario/apps/sucursal/models.py
+++ concesionario/concesionario/apps/sucursal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Sucursal(models.models):
+	id_sucursal = models.AutoField(primary_key=True)
+	nombre = models.ChartField(null = True, blank = True, max_lenght = 20)
+	direccion = models.ChartField(null = True, blank = True, max_lenght = 50)
+	telefono = models.ChartField(null = True, blank = True, max_lenght = 10)
+	ciudad = models.ChartField(null = True, blank = True, max_lenght = 50)
+	activo = models.BooleanField(null = True, blank = True, default = True)
