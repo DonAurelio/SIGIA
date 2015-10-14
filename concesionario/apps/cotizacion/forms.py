c8058f1adeac6f923ddb3e1ddaa5dccd9a07bbdd
lisafbe <lisabetanco@gmail.com>
2015-10-13 23:50:51 -0500

    pruebas de template crear para el campo de fecha

--- concesionario/apps/cotizacion/forms.py
+++ concesionario/apps/cotizacion/forms.py
@@ -1,9 +1,12 @@
 # -*- encoding: utf-8 -*-
 
+from django import forms
 from django.views.generic.edit import CreateView 
 from .models import Cotizacion 
  
+ 
 class CrearCotizacion(CreateView): 
+	 
 	template_name = 'cotizacion/includes/crear.html'
 	model = Cotizacion 
 	fields = ['empleado', 'cliente', 'vehiculo', 'fecha','fecha_vencimiento', 'forma_pago'] 
