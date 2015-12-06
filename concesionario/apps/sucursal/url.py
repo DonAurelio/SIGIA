f2025fd55dffb3bcf603653fa928cd04b47daf07
BryanTabarez <userbryan@gmail.com>
2015-12-06 00:37:44 -0500

    templates sucursal

--- concesionario/apps/sucursal/url.py
+++ concesionario/apps/sucursal/url.py
@@ -8,10 +8,10 @@ from .views import ListaSucursales
 urlpatterns = [
 
     # redirecciona a la pagina de creacion de sucursal
-    url(r'^sucursal/crear$', CrearSucursal.as_view(), name='crear-sucursal'),
+    url(r'^sucursal/crear$', CrearSucursal.as_view(), name='crear'),
     # redirecciona a pagina para actualizacion de una sucursal
-    url(r'^sucursal/(?P<pk>\d+)/$', ActualizarSucursal.as_view(), name='actualizar-sucursal'), 
+    url(r'^sucursal/(?P<pk>\d+)/$', ActualizarSucursal.as_view(), name='actualizar'), 
     # redirecciona a pagina que despliega el listado de sucursales
-    url(r'^sucursal/listado$', ListaSucursales.as_view(), name='lista-sucursales'),
+    url(r'^sucursal/listado$', ListaSucursales.as_view(), name='listar'),
     
 ]
