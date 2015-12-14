fcd7a36a52a46f45257f8aa6357035a0f9c8e5a1
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2015-12-14 08:19:15 -0500

    Comprovando submit de formuarios

--- concesionario/static/concesionario/js/gerente.js
+++ concesionario/static/concesionario/js/gerente.js
@@ -1,8 +1,26 @@
 /* Get HTML data form menu items using the href atrribute of the clicked button */
-$("li > a").click(function (event) {
+/*$("li > a").click(function (event) {
 	event.preventDefault();
     var addressValue = $(this).attr("href");
     $.get(addressValue, function(data, status){
 		$("#content").html(data);
 	});
+});
+*/
+$("a").click(function (event) {
+	event.preventDefault();
+    var addressValue = $(this).attr("href");
+    $.get(addressValue, function(data, status){
+		$("#content").html(data);
+	});
+});
+
+$("button").click(function(event){
+	event.preventDefault();
+    $.post("/sucursal/crear",
+    $("#content").html()
+    ,
+    function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
 });
