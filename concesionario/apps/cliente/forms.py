f6986094ae97f9cfcd34ae47e79c647876363f91
lisafbe <lisabetanco@gmail.com>
2015-12-18 14:28:27 -0500

    Gestionar cliente en perfil vendedor

--- concesionario/apps/cliente/forms.py
+++ concesionario/apps/cliente/forms.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
-
+from django.core.urlresolvers import reverse_lazy, reverse
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView
 from .models import Cliente
 
@@ -8,8 +9,16 @@ class CrearCliente(CreateView):
 	model = Cliente
 	fields = ['identificacion', 'nombre', 'apellido', 'ciudad',
 	'departamento', 'telefono', 'celular', 'email']
+	success_url = reverse_lazy('cliente:listar') 
 
 class ActualizarCliente(UpdateView): 
     model = Cliente 
     fields = ['identificacion', 'nombre', 'apellido', 'ciudad',
     'departamento', 'telefono', 'celular', 'email']
+    success_url = reverse_lazy('cliente:listar')
+
+    # -*- encoding: utf-8 -*-
+
+
+ 
+ 
