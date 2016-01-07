c0bc8f3a32a206b01a7b938e61c601edeb19cd14
lisafbe <lisabetanco@gmail.com>
2016-01-07 16:50:40 -0500

    correccion-no borrar

--- concesionario/apps/venta/forms.py
+++ concesionario/apps/venta/forms.py
@@ -1,12 +1,15 @@
 # -*- encoding: utf-8 -*-
-
+from django.core.urlresolvers import reverse_lazy, reverse
+from django import forms
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView
 from .models import Venta
  
 class CrearVenta(CreateView):
     model = Venta
     fields = ['empleado', 'cliente', 'vehiculo', 'fecha_venta', 'descuento', 'precio_venta', 'forma_pago']
-
+    success_url = reverse_lazy('venta:listar') 
 class ActualizarVenta(UpdateView):
     model = Venta
     fields = ['empleado', 'cliente', 'vehiculo', 'fecha_venta', 'descuento', 'precio_venta', 'forma_pago']
+    success_url = reverse_lazy('venta:listar') 
