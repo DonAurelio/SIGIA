7f89e0ccdaf1388ab64f1ee03a13ef650b8d2136
lisafbe <lisabetanco@gmail.com>
2016-02-05 14:40:06 -0500

    Reporte ventas por sucursal

--- concesionario/apps/reporte/forms.py
+++ concesionario/apps/reporte/forms.py
@@ -24,4 +24,27 @@ class ReporteVendedores(View):
 		for m in ventasPorEmpleado:
 			m['empleado'] = str(Empleado.objects.get(id=m["empleado"]).user.first_name)+" "+str(Empleado.objects.get(id=m["empleado"]).user.last_name)+"("+str(Empleado.objects.get(id=m["empleado"]).sucursal)+")"
 		print ventasPorEmpleado[1]
-		return render (request, 'reporte/reporte_form.html',{'ventasPorEmpleado':ventasPorEmpleado})
+		return render (request, 'reporte/reporte_VentasVendedor.html',{'ventasPorEmpleado':ventasPorEmpleado})
+
+
+class ReporteVentasSucursal(View):
+
+	def get(self, request, **kwargs):
+		ventasPorSucursal =Venta.objects.values("empleado__sucursal__nombre").annotate(cuantos=Count('empleado_id')).order_by(Coalesce('cuantos', 'cuantos').desc())
+		ventas = []
+		for x in range(len(ventasPorSucursal)):
+			if x == 0:
+				ven = {}
+				ven['sucursal']=str(ventasPorSucursal[x]['empleado__sucursal__nombre'])
+				ven['cuantos']=ventasPorSucursal[x]['cuantos']
+				ventas.append(ven)
+			elif ventasPorSucursal[x]['empleado__sucursal__nombre'] == ventasPorSucursal[x-1]['empleado__sucursal__nombre']:
+				ventas[x-1]['cuantos']=ventas[x-1]['cuantos']+ventasPorSucursal[x]['cuantos']
+			else:
+				ven = {}
+				ven['sucursal']=str(ventasPorSucursal[x]['empleado__sucursal__nombre'])
+				ven['cuantos']=ventasPorSucursal[x]['cuantos']
+				ventas.append(ven)
+
+		print ventas
+		return render (request, 'reporte/reporte_VentasSucursal.html',{'ventas':ventas})
--- concesionario/apps/reporte/url.py
+++ concesionario/apps/reporte/url.py
@@ -2,6 +2,10 @@
 
 from django.conf.urls import include, url
 from .forms import ReporteVendedores
+from .forms import ReporteVentasSucursal
 
 urlpatterns = [
-	url(r'^reporte/MejoresVendedores/$',ReporteVendedores.as_view(), name='MejoresVendedores'),]
+	url(r'^reporte/MejoresVendedores/$',ReporteVendedores.as_view(), name='MejoresVendedores'),
+	url(r'^reporte/VentasSucursal/$',ReporteVentasSucursal.as_view(), name='VentasSucursal'),
+
+	]
