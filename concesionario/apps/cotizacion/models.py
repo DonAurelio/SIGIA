36fcfe77f769eacb033101303c3d021f20a7da24
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2016-02-19 12:22:02 -0500

    Modificando detalle del cliente

--- concesionario/apps/cotizacion/models.py
+++ concesionario/apps/cotizacion/models.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 from django.db import models
-from datetime import datetime    
+from datetime import datetime
 from apps.empleado.models import Empleado
 from apps.cliente.models import Cliente
 from apps.vehiculo.models import Vehiculo
@@ -22,12 +22,12 @@ forma_pago_choices = (
 #
 class Cotizacion(models.Model):
 	"""Define la organizacion del los datos de una cotizacion en la base de datos."""
-	
-	#Empleado que realiza la cotizacion, relacion uno a muchos 
+
+	#Empleado que realiza la cotizacion, relacion uno a muchos
 	empleado= models.ForeignKey(Empleado,default=None)
-	#Cliente que solicita la cotizacion, relacion uno a muchos 
+	#Cliente que solicita la cotizacion, relacion uno a muchos
 	cliente= models.ForeignKey(Cliente,default=None)
-	#Vehiculo cotizado, relacion uno a muchos 
+	#Vehiculo cotizado, relacion uno a muchos
 	vehiculo= models.ForeignKey(Vehiculo,default=None)
 	#Fecha en que se realiza la cotizacion
 	fecha=models.DateField(auto_now_add=True,blank=True,null=True)
@@ -37,21 +37,16 @@ class Cotizacion(models.Model):
 	forma_pago = models.CharField(max_length=20,choices=forma_pago_choices, default=EFECTIVO)
 	#Estado de la cotizacion, Activa/inactiva
 	habilitado = models.BooleanField(default = True)
-	
-	#Permite hacer modificaciones agregadas a la representacion del modelo 
+
+	#Permite hacer modificaciones agregadas a la representacion del modelo
 	class Meta:
 		ordering = ['fecha']
 		verbose_name_plural = "Cotizaciones"
 
-	#Permite determinar una representacion en string del objeto  
+	#Permite determinar una representacion en string del objeto
 	def __str__(self):
 		return self.fecha
 
 	#Permite determinar una represetacion en string para el objeto (Esto es para versiones de Python 2)
 	def __unicode__(self):
-		return self.fecha 
- 
-
-
-
- 
+		return self.fecha
